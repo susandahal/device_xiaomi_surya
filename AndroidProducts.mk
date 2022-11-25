0fb49bae1d74fe57020050264d810afb5470528c
susandahal <susandahal53@gmail.com>
2022-11-25 08:13:27 +0545

    surya:Bringup PixysOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_surya.mk
+    $(LOCAL_DIR)/pixys_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_surya-user \
-    lineage_surya-userdebug \
-    lineage_surya-eng
+    pixys_surya-user \
+    pixys_surya-userdebug \
+    pixys_surya-eng
