b6bf6691a60d2ebf938a21208588510036b219fb
susandahal <susandahal53@gmail.com>
2023-03-06 08:58:55 +0545

    surya:PixysOS

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 Reloaded OS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pixys_surya.mk
+
+COMMON_LUNCH_CHOICES := \
+    pixys_surya-user \
+    pixys_surya-userdebug \
+    pixys_surya-eng
---
+++ pixys_surya.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2018-2023 ReloadedOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from surya device
+$(call inherit-product, device/xiaomi/surya/device.mk)
+
+# Inherit some common ReloadedOS stuff.
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+
+PRODUCT_NAME := pixys_surya
+PRODUCT_DEVICE := surya
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := M2007J20CG
+PRODUCT_MANUFACTURER := Xiaomi
+
+#BootAnimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
